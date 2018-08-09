30c142ac6de87a83e85e7e10d026f9ea8387d46b
Na <lette1394+github@gmail.com>
2018-08-10 03:29:54 +0900

    ADD chat pagination

--- src/main/java/com/web2team/graphql/repository/ChatRepository.java
+++ src/main/java/com/web2team/graphql/repository/ChatRepository.java
@@ -1,8 +1,12 @@
 package com.web2team.graphql.repository;
 
 import com.web2team.graphql.model.Chat;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ChatRepository extends CrudRepository<Chat, Long> {
   Iterable<Chat> getAllByChatThreadIdEquals(Long chatThreadId);
+
+  Page<Chat> getAllByChatThreadIdEquals(Long chatThreadId, Pageable pageable);
 }
--- src/main/java/com/web2team/graphql/resolver/Chat/ChatQuery.java
+++ src/main/java/com/web2team/graphql/resolver/Chat/ChatQuery.java
@@ -5,7 +5,11 @@ import com.web2team.graphql.model.Chat;
 import com.web2team.graphql.repository.ChatRepository;
 import graphql.schema.DataFetchingEnvironment;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -14,5 +18,11 @@ public class ChatQuery implements GraphQLQueryResolver {
 
   public Iterable<Chat> getAllMessages(Long chat_thread_id, DataFetchingEnvironment env) {
     return chatRepository.getAllByChatThreadIdEquals(chat_thread_id);
+  }
+
+  public List<Chat> getMessages(Long chat_thread_id, int page, int size) {
+    Pageable pageable = PageRequest.of(page, size);
+
+    return chatRepository.getAllByChatThreadIdEquals(chat_thread_id, pageable).getContent();
   }
 }
